ed070dedc339f25924919d095369b2692f57df4c
Dudgit <bence.dudas20@gmail.com>
2025-02-16 21:29:45 +0000

    changed to scale

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import numpy as np
 from utils.model import Net
 from omegaconf import OmegaConf
 import datetime
+from itertools import product
+
 
 def logMetadata(net,config):
     currDat = datetime.datetime.now().strftime('%Y-%b-%d %H:%M:%S')
@@ -14,6 +16,16 @@ def logMetadata(net,config):
     net.add_logger(writer)
     with open(conflog,'w') as f:
         OmegaConf.save(config,f.name)
+
+def get_combinations(nested_dict):
+    keys, values = zip(*nested_dict.items())
+    combinations = [dict(zip(keys, combination)) for combination in product(*values)]
+    return combinations
+
+def get_all_combinations(nested_dict):
+    flat_dict = {f"{outer_key}.{inner_key}": inner_value for outer_key, inner_dict in nested_dict.items() for inner_key, inner_value in inner_dict.items()}
+    combinations = get_combinations(flat_dict)
+    return combinations
 
 def main(config,device:str ='cpu'):
     net = Net(**config.modelParams)
@@ -29,9 +41,12 @@ def main(config,device:str ='cpu'):
 
 if __name__ == '__main__':
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    config = OmegaConf.load('config.yaml')
-    for hsize in config.modelParams.hidden_size:
-        cfg = config.copy()
-        cfg.modelParams.hidden_size = hsize
+    config = OmegaConf.load('configs/const.yaml')
+    changevars = OmegaConf.load('configs/multivar.yaml')
+    combinations = get_all_combinations(changevars)
+    for c in combinations:
+        cflist = [ f'{key}={value}' for key, value in c.items()]
+        dotlist = OmegaConf.from_dotlist(cflist)
+        cfg = OmegaConf.merge(config, dotlist)
         main(config=cfg,device=device)
 
